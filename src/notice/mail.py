b4093d16130d140dd1a0b2522696f5a40a0f07b8
jiang-jing-jiu <77264352+jiang-jing-jiu@users.noreply.github.com>
2022-07-19 20:41:31 +0800

    Update mail.py

--- src/notice/mail.py
+++ src/notice/mail.py
@@ -64,7 +64,7 @@ def format_content(cves):
 <br/><br/>
 ++++++++++++++++++++++++++++++++++++++++++++++
 <br/>
-<font color="red">【情报收集与播报支持】</font> https://lyy289065406.github.io/threat-broadcast/
+<font color="red">【情报收集与播报支持】</font> https://github.com/jiang-jing-jiu/threat-broadcast
 '''
     src_infos = []
     cve_infos = []
